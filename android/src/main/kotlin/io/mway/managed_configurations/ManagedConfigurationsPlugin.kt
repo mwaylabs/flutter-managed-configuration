0b02272e8a1419a80ed43676e0c1bfcc44aaf321
Sander Roest <s.roest@dalosy.com>
2021-12-30 14:59:59 +0100

    Convert the whole bundle to json, instead of only the keys on level 1.

--- android/src/main/kotlin/io/mway/managed_configurations/ManagedConfigurationsPlugin.kt
+++ android/src/main/kotlin/io/mway/managed_configurations/ManagedConfigurationsPlugin.kt
@@ -2,8 +2,8 @@ package io.mway.managed_configurations
 
 import android.app.Activity
 import android.content.*
-import android.util.Log
 import androidx.annotation.NonNull
+import com.google.gson.GsonBuilder
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -13,104 +13,85 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import org.json.JSONException
-import org.json.JSONObject
 
 
 /** ManagedConfigurationsPlugin */
 class ManagedConfigurationsPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
-    private lateinit var channel: MethodChannel
+    private var channel: MethodChannel? = null
+    private var eventChannel: EventChannel? = null
+    private var eventSink: EventSink? = null
+    private var activity: Activity? = null
 
-    ///
-    ///EventChannel which will be used to provide Event to Flutter part
-    ///
-    private lateinit var eventChannel: EventChannel
-    private lateinit var eventStreamHandler: EventChannel.StreamHandler
+    private val restrictionsReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            eventSink?.success(getApplicationRestrictions())
+        }
+    }
 
-
-    private lateinit var context: Context
-    private lateinit var activity: Activity
-
+    private val gson = GsonBuilder().registerTypeAdapterFactory(BundleTypeAdapterFactory())
+        .create()
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        context = flutterPluginBinding.applicationContext
+        channel =
+            MethodChannel(flutterPluginBinding.binaryMessenger, "managed_configurations_method")
+        channel!!.setMethodCallHandler(this)
 
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "managed_configurations_method")
-        channel.setMethodCallHandler(this)
-
-        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "managed_configurations_event");
-
-
-        val restrictionsFilter = IntentFilter(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED)
-
-        eventChannel.setStreamHandler(object : EventChannel.StreamHandler {
-            override fun onListen(p0: Any?, p1: EventSink) {
-
-                //Create an broadcast receiver
-                val restrictionsReceiver = object : BroadcastReceiver() {
-                    override fun onReceive(context: Context, intent: Intent) {
-                        var restrictionManager = activity.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager
-                        // Get the current configuration bundle
-                        val appRestrictions = restrictionManager.applicationRestrictions
-                        val json = JSONObject()
-                        val keys: Set<String> = appRestrictions.keySet()
-                        for (key in keys) {
-                            try {
-                                json.put(key, JSONObject.wrap(appRestrictions.get(key)))
-                            } catch (e: JSONException) {
-                                Log.d("Managed_Configuration", "Failed to put value with key: $key into JSONObject")
-                            }
-                        }
-                        p1.success(json.toString())
-                    }
+        eventChannel =
+            EventChannel(flutterPluginBinding.binaryMessenger, "managed_configurations_event")
+        eventChannel!!.setStreamHandler(
+            object : EventChannel.StreamHandler {
+                override fun onListen(args: Any?, sink: EventChannel.EventSink) {
+                    eventSink = sink
                 }
-                context.registerReceiver(restrictionsReceiver, restrictionsFilter)
+
+                override fun onCancel(args: Any?) {
+                    eventSink = null
+                }
             }
-
-            override fun onCancel(p0: Any?) {}
-        }
         )
-
-
+        flutterPluginBinding.applicationContext.registerReceiver(
+            restrictionsReceiver,
+            IntentFilter(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED)
+        )
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        var restrictionManager = activity.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager
-        if (call.method == "getManagedConfigurations") {
-            val json = JSONObject()
-            val keys: Set<String> = restrictionManager.applicationRestrictions.keySet()
-            for (key in keys) {
-                try {
-                    json.put(key, JSONObject.wrap(restrictionManager.applicationRestrictions.get(key)))
-                } catch (e: JSONException) {
-                    Log.d("Managed_Configuration", "Failed to put value with key: $key into JSONObject")
-                }
-            }
-            result.success(json.toString())
-        } else {
-            result.notImplemented()
+        when (call.method) {
+            "getManagedConfigurations" -> result.success(getApplicationRestrictions())
+            else -> result.notImplemented()
         }
     }
 
+
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-    }
-
-    override fun onDetachedFromActivity() {
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        channel?.setMethodCallHandler(null)
+        eventChannel?.setStreamHandler(null)
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        activity = binding.activity;
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    private fun getApplicationRestrictions(): String? {
+        val restrictionManager =
+            activity?.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager?
+                ?: return null
+        // Get the current configuration bundle
+        val applicationRestrictions = restrictionManager.applicationRestrictions
+        //val json = JSONObject()
+        return gson.toJson(applicationRestrictions).toString()
     }
 }
 
